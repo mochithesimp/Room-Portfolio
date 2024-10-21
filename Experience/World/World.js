121d489862232acddbf7f3c97cbba677afe8f377
Mochi <antivirused29112003@gmail.com>
2024-10-21 15:17:14 +0700

    Add model and Migrate project to older version of Vite

--- Experience/World/World.js
+++ Experience/World/World.js
@@ -10,8 +10,11 @@ export default class World {
         this.scene = this.experience.scene;
         this.canvas = this.experience.canvas;
         this.camera = this.experience.camera;
+        this.resources = this.experience.resources;
 
-        this.room = new Room();
+        this.resources.on("ready", () => {
+            this.room = new Room();
+        })
     }
 
     resize() {
