10211603494b136dc3fed8addaa21a6a1514b1e0
Mochi <antivirused29112003@gmail.com>
2024-10-24 21:16:07 +0700

    Adjsut camera and remove the test grid

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -24,30 +24,33 @@ export default class Camera {
     }
 
     createOrthographicCamera() {
-        // this.orthographicCamera = new THREE.OrthographicCamera(
-        //     (-this.sizes.aspect * this.sizes.frustrum) / 2,
-        //     (this.sizes.aspect * this.sizes.frustrum) / 2,
-        //     this.sizes.frustrum / 2,
-        //     -this.sizes.frustrum / 2,
-        //     -100,
-        //     100
-        // );
+        this.orthographicCamera = new THREE.OrthographicCamera(
+            (-this.sizes.aspect * this.sizes.frustrum) / 2,
+            (this.sizes.aspect * this.sizes.frustrum) / 2,
+            this.sizes.frustrum / 2,
+            -this.sizes.frustrum / 2,
+            -100,
+            100
+        );
 
-        this.orthographicCamera = new THREE.PerspectiveCamera(35, this.sizes.aspect, 0.1, 1000);
+        this.orthographicCamera.position.y = 3.5;
+        this.orthographicCamera.position.z = 5;
+        this.orthographicCamera.rotation.x = -Math.PI / 6;
+
 
         this.scene.add(this.orthographicCamera)
 
-        this.helper = new THREE.CameraHelper(this.orthographicCamera);
-        this.scene.add(this.helper);
+        // this.helper = new THREE.CameraHelper(this.orthographicCamera);
+        // this.scene.add(this.helper);
 
         const size = 20;
         const divisions = 20;
 
-        const gridHelper = new THREE.GridHelper(size, divisions);
-        this.scene.add(gridHelper);
+        // const gridHelper = new THREE.GridHelper(size, divisions);
+        // this.scene.add(gridHelper);
 
-        const axesHelper = new THREE.AxesHelper(10);
-        this.scene.add(axesHelper);
+        // const axesHelper = new THREE.AxesHelper(10);
+        // this.scene.add(axesHelper);
     }
 
     setOrbitControls() {
@@ -72,10 +75,10 @@ export default class Camera {
         //console.log(this.perspectiveCamera.position)
         this.controls.update();
 
-        this.helper.matrixWorldNeedsUpdate = true;
-        this.helper.update();
-        this.helper.position.copy(this.orthographicCamera.position);
-        this.helper.rotation.copy(this.orthographicCamera.rotation);
+        // this.helper.matrixWorldNeedsUpdate = true;
+        // this.helper.update();
+        // this.helper.position.copy(this.orthographicCamera.position);
+        // this.helper.rotation.copy(this.orthographicCamera.rotation);
 
     }
 }
--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -67,7 +67,7 @@ export default class Room {
         window.addEventListener("mousemove", (e) => {
             this.rotation =
                 ((e.clientX - window.innerWidth / 2) * 2) / window.innerWidth;
-            this.lerp.target = this.rotation;
+            this.lerp.target = this.rotation * 0.1;
         })
     }
 
