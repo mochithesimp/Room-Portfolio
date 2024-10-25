943a4eec4fb8ea145cc62635ae319326deb6e313
Mochi <antivirused29112003@gmail.com>
2024-10-25 10:35:03 +0700

    Add theme

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -1,6 +1,7 @@
 import * as THREE from "three"
 import Experience from "../Experience.js";
 import GSAP from "gsap"
+import { RectAreaLightHelper } from "three/examples/jsm/helpers/RectAreaLightHelper.js";
 
 
 export default class Room {
@@ -51,7 +52,24 @@ export default class Room {
                     map: this.resources.items.screen,
                 });
             }
-        })
+        });
+
+        const width = 0.5;
+        const height = 0.7;
+        const intensity = 1;
+        const rectLight = new THREE.RectAreaLight(
+            0xffffff,
+            intensity,
+            width,
+            height,
+        );
+        rectLight.position.set(7.68244, 7, 0.5);
+        rectLight.rotation.x = -Math.PI / 2;
+        rectLight.rotation.z = Math.PI / 4;
+        this.actualRoom.add(rectLight);
+
+        //const rectLightHelper = new RectAreaLightHelper(rectLight);
+        //rectLight.add(rectLightHelper);
 
         this.scene.add(this.actualRoom);
         this.actualRoom.scale.set(0.11, 0.11, 0.11);
